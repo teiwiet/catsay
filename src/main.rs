4342ef9b241b95c47a663fae16d4fc2000b79b50
teiwiet <omegalmao1.3@gmail.com>
2024-01-15 19:14:09 +0700

    update

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,16 @@
-use std::env;
 use structopt::StructOpt;
-
-#[derive(StructOpt)]
-struct Options{
-    message : String,
-    #[structopt(short="d",long = "dead")]
-    dead : bool
+#[derive(Debug,StructOpt)]
+struct Option{
+    message : String
 }
 
-
-fn main(){
-    let options = Options::from_args();
+fn main() {
+    let options = Option::from_args();
     let message = options.message;
-    // let mut args : Vec<String> = env::args().collect();
-    // println!("{}", args[1]);
-    if message.to_lowercase() == "woof"{
-        eprintln!(":-))");
-        return
-    }
-    let eye = if options.dead {
-        "x"
-    }
-    else {
-        "O"
-    };
-    println!("{}", message);
+    println!("{}",message);
     println!(" \\");
     println!(" \\");
     println!("  /\\_/\\");
-    println!(" ( {eye} {eye} )");
+    println!(" ( o o )");
     println!(" =( I )=");
 }
